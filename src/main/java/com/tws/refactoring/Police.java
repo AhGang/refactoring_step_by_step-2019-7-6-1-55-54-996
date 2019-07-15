da7a25c6886a8d7be8b185824f743abbbcd43bb2
AhGang <474494594@qq.com>
2019-07-15 14:05:45 +0800

    识别代码的坏味道

--- src/main/java/com/tws/refactoring/Police.java
+++ src/main/java/com/tws/refactoring/Police.java
@@ -1,8 +1,9 @@
 package com.tws.refactoring;
 
 public class Police {
+    private static int driverLimitedAge = 18;
     public boolean checkDriver(Driver driver) {
-        if(driver.age >= 18) return true;
-        else return false;
+
+        return (driver.getAge() >= Police.driverLimitedAge)?true:false;
     }
 }
